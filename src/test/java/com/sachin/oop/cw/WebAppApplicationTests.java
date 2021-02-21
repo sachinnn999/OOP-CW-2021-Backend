41d8ddf5557fcb9f115b90cf8c56b4b6ca9941a1
sachinnn999 <sachin.rds99@gmail.com>
2021-02-22 00:31:12 +0530

    Test Classes changes

--- src/test/java/com/sachin/oop/cw/WebAppApplicationTests.java
+++ src/test/java/com/sachin/oop/cw/WebAppApplicationTests.java
@@ -1,9 +1,10 @@
 package com.sachin.oop.cw;
 
+import com.sachin.oop.cw.Controller.Main;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Main.class)
 class WebAppApplicationTests {
 
 	@Test
